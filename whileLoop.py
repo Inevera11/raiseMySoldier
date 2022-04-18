c80dd1e39fb77ad9ab1e0933c25902487d600e14
Inevera11 <zuzannabozek2002@gmail.com>
2022-04-18 16:38:08 +0200

    Mati

--- whileLoop.py
+++ whileLoop.py
@@ -13,22 +13,44 @@ def nextTenNumbers(n):
 def cutFirstLetter(word: str) -> str:
     while word:
         print(word, end=' ')
-        word = word[1: ]
-# cutFirstLetter("Kocham")
+        word = word[:-1]
+    print()
+    return word
+cutFirstLetter("Alleluja")
 
 def is_prime(num):
+
     for i in range(2, num):
         if num % i == 0:
             return False
     return True
 def get_primes(max_num):
+    # output = []
+    # for i in range(2, max_num+1):
+    #     if is_prime(i):
+    #         output.append(i)
+    # return output
     output = []
-    for i in range(2, max_num+1):
+    i = 2
+    while i <= max_num:
         if is_prime(i):
             output.append(i)
     return output
 # print(get_primes(11))
 
+def get_primes_better(max_num):
+    primes = []
+    for i in range(2, max_num+1):
+        is_num_prime = True
+        for prime in primes:
+            if i % prime == 0:
+                is_num_prime = False
+                break
+        if is_num_prime:
+            primes.append(i)
+    return primes
+
+print(get_primes_better(11))
 
 def pattern(startPoint,lenght):
     nextX = 0
