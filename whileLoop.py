3da4972c3d0c5e41f98f7dd9893386c6c556f59b
Inevera11 <zuzannabozek2002@gmail.com>
2022-04-18 10:08:39 +0200

    nicer

--- whileLoop.py
+++ whileLoop.py
@@ -17,11 +17,11 @@
 
 def pattern(startPoint,lenght):
     nextX = 0
-    while lenght>0:
-        if nextX ==0:
+    while lenght > 0:
+        if nextX == 0:
             sum = str(startPoint)
         else:
-            sum =  str(nextX) +" "+ str(startPoint)
+            sum =  str(nextX) + " " + str(startPoint)
         print(sum)
         nextX = sum
         startPoint += 1
