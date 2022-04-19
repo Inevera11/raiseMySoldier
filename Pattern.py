2772fb8ff308e356edad06391f718d8e95694c12
Inevera11 <zuzannabozek2002@gmail.com>
2022-04-19 11:38:23 +0200

    Pyramid

--- Pattern.py
+++ Pattern.py
@@ -10,3 +10,47 @@ def pattern(startPoint,lenght):
         startPoint += 1
         lenght -= 1
 # pattern(6, 9)
+
+def pyramid(topNumber,lenght):
+    if topNumber<0:
+        topNumber = topNumber*(-1)
+    loop = lenght
+    while loop>0:
+        if loop == lenght:
+            print(loop*" " + str(topNumber))
+        else:
+            greaterX = topNumber+1*(lenght-loop)
+            lesserX = topNumber-1*(lenght-loop)
+            if loop ==lenght-1:
+                middle = str(topNumber)
+            if lesserX<0:
+                lesserX = lesserX*(-1)
+            string = str(lesserX) + middle + str(greaterX)
+            if greaterX>10:
+                string = str(lesserX) + middle + str(greaterX)[1:]
+            print(loop*" "+ string )
+            middle = string
+        loop -= 1
+# pyramid(10,15)
+# pyramid(-1,4)
+
+def symetricPyramid(top, lenght):
+    if top<0:
+        top = top*(-1)
+    loop = lenght
+    while loop>0:
+        if loop == lenght:
+            print(loop*" " + str(top))
+        else:
+            sideNumber = top - 1
+            if sideNumber < 0:
+                sideNumber = sideNumber*(-1)
+            if loop ==lenght-1:
+                middle = str(top)
+            string = str(sideNumber) + middle + str(sideNumber)
+            print(loop*" "+string)
+            middle = string
+            top -= 1
+        loop -= 1
+# symetricPyramid(6,7)
+# symetricPyramid(9,11)
